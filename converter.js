8139a44606a9730320746bd07dfa96dfac45d43b
BukkitmanPlays-MCPE <chamacoselles@gmail.com>
2016-10-21 16:44:26 -0700

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,11 +1,43 @@
-var converter = document.getElementById("convert")
+var converter = {
+	// First we have Variables, which are the Objects that are in the page, or in this case, the iframe from the actual web page.
+	convert: document.getElementById("convert"),
 
-function convert() {
-	alert("Conversion Successful!")
+	// These are the left Options:
+	Import: document.getElementById("import"),
+	Paste: document.getElementById("paste"),
+
+	// These are the right Options:
+	WithLua: document.getElementById("wl"),
+	WithoutLua: document.getElementById("wol"),
+
+	// We finnished the variables, so now we have the functions!!!
+	convert: function() {
+		alert("Conversion Successful!")
+	},
 }
 
-function showLink() {
-	var link = 'to add the converter to your website, you will need to paste this in your web page: <iframe src="Application.html"style="width: 100%; height: 502px; border-width: 0;"><h2>The converter will only work if your web browser supports iframes</h2></iframe>';
+function selectOption(side, option) {
+	if (side == "left") {
 
-	alert(link);
+		if (option == 1) {
+			converter.Import.backgroundColor = '#b1e3fa';
+			converter.Paste.backgroundColor = '#a0d2e9';
+		} else if (option == 2) {
+			converter.buttons.Paste.backgroundColor = '#b1e3fa';
+			converter.buttons.Import.backgroundColor = '#a0d2e9';
+		}
+
+	} else if (side == "right") {
+
+		if (option == 1) {
+			converter.WithLua.backgroundColor = '#b1e3fa';
+			converter.WithoutLua.backgroundColor = '#a0d2e9';
+		} else if (option == 2) {
+			converter.WithoutLua.backgroundColor = '#b1e3fa';
+			converter.WithLua.backgroundColor = '#a0d2e9';
+		}
+
+	} else {
+		alert("function has not been performed proplery...")
+	}
 }
